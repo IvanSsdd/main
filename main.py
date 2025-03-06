ff0fc308bab153e39727cf433090242c413c6f97
IvanSsdd <169038313+IvanSsdd@users.noreply.github.com>
2025-03-06 12:10:52 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+def reverse_string(text):
+    return text[::-1]
+
+    
+    # input_text = "привет"
+    # reversed_text = reverse_string(input_text)
+    # print("Обратная строка:", reversed_text)
