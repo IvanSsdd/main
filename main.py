599e9ae98008573203c5a514977ba1c120704174
IvanSsdd <169038313+IvanSsdd@users.noreply.github.com>
2025-04-23 18:23:23 +0300

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-def reverse_string(text):
-    return text[::-1]
-
-    
-    # input_text = "привет"
-    # reversed_text = reverse_string(input_text)
-    # print("Обратная строка:", reversed_text)
